ebae23f212e76e85a630af8de781259164790c9f
bogdan_popov1 <bogdan_popov17@mail.ru>
2023-11-18 11:05:55 +0300

    исправлены ошибки входа и действий в аккаунте админа

--- Program.cs
+++ Program.cs
@@ -4,10 +4,15 @@
 Admin admin__AdultDepartment = new Admin("1", "1", "Взрослое отделение", "Никотяй Игорь Андреевич");
 Admin admin__ChildrenDepartment = new Admin("adminChildrenDepartment", "childrenDepartment2023", "Детское отделение", "Чумаков Данил Дмитриевич");
 
-List <Doctor> doctorsList = new List<Doctor>()
+List <Doctor> doctorsList__AdultDepartment = new List<Doctor>()
 {
     //new Doctor("2", "2938483", "adult", "terapevt", "zhopa kota"),
     //new Doctor("3", "2938483", "adult", "terapevt", "zhopa kota")
+};
+
+List<Doctor> doctorsList__ChildrenDepartment = new List<Doctor>()
+{
+
 };
 
 bool trigger1 = true;
@@ -17,83 +22,30 @@ Console.WriteLine("Добро пожаловать в нашу больницу!
 
 while (trigger1)
 {
-    Console.WriteLine("Что бы вы хотели сделать?\n" + "1. Войти как админ.\n" + "2. Войти как врач.\n" + "3. Записаться на приём.\n" + "4. Выйти из приложения");
+    Console.WriteLine("Что бы вы хотели сделать?\n" + "1. Войти как админ.\n" + "2. Войти как врач.\n" + "3. Записаться на приём.\n" + "4. Выйти из приложения.");
 
     int actionStart = Convert.ToInt32(Console.ReadLine());
+
+    trigger2 = true;
 
     switch (actionStart)
     {
         case 1:
             Console.Clear();
 
-            Admin admin = SignInAdmin(admin__AdultDepartment, admin__ChildrenDepartment);
-
-
-
             while (trigger2)
             {
-                Console.Clear();
-
-                Console.WriteLine(admin.FullName.ToUpper() + "\n");
-                Console.WriteLine("Что бы вы хотели сделать?\n" + "1. Посмотреть список врачей.\n" + "2. Добавить врача.\n" + "3. Удалить врача.\n" + "4. Поменять пароль.\n" + "5. Выйти из аккаунта");
-                int actionAdmin = Convert.ToInt32(Console.ReadLine());
-
-                switch (actionAdmin)
+                
+                if (SignInAdmin(admin__AdultDepartment, admin__ChildrenDepartment) == true)
                 {
-                    case 1:
-                        Console.Clear();
-
-                        if (doctorsList.Count > 0)
-                        {
-                            foreach (Doctor doctor in doctorsList)
-                            {
-                                Console.WriteLine($"{doctor.FullName}  |  {doctor.Department}  |  {doctor.Specialization}\n");
-                            }
-
-                        }
-                        else
-                        {
-                            Console.WriteLine("Список врачей пуст. Пора принимать на работу!");
-                        }
-
-                        Console.WriteLine("Нажмите 1, чтобы выйти.");
-                        string exit = Console.ReadLine();
-
-                        break;
-
-
-                    case 2:
-                        Console.Clear();
-
-                        if (admin.Department == "Взрослое отделение")
-                        {
-                            Doctor doctor = CreateDoctor__AdultDepartment();
-                            doctorsList.Add(doctor);
-                        }
-                        else if (admin.Department == "Детское отделение")
-                        {
-                            Doctor doctor = CreateDoctor__ChildrenDepartment();
-                            doctorsList.Add(doctor);
-                        }
-
-
-                        break;
-
-                    case 3:
-                        Console.Clear();
-
-                        break;
-
-                    case 4:
-                        Console.Clear();
-
-                        break;
-
-                    case 5:
-                        trigger2 = false;
-                        Console.Clear();
-                        break;
+                    Console.Clear();
+                    trigger2 = AdminAccount(admin__AdultDepartment, doctorsList__AdultDepartment, trigger2);
+                } else if (SignInAdmin(admin__AdultDepartment, admin__ChildrenDepartment) == false)
+                {
+                    Console.Clear();
+                    trigger2 = AdminAccount(admin__ChildrenDepartment, doctorsList__ChildrenDepartment, trigger2);
                 }
+                
             }
 
             break;
@@ -121,7 +73,7 @@ while (trigger1)
 
 
 
-Admin SignInAdmin(Admin adminAdult, Admin adminChildren)
+bool SignInAdmin(Admin adminAdult, Admin adminChildren)
 {
     Console.WriteLine("ВХОД ОТ ИМЕНИ АДМИНИСТРАТОРА\n");
 
@@ -149,10 +101,10 @@ Admin SignInAdmin(Admin adminAdult, Admin adminChildren)
 
     if (login == checkLogin__AdultDepartment)
     {
-        return adminAdult;
+        return true;
     } else
     {
-        return adminChildren;
+        return false;
     }
 }
 
@@ -197,4 +149,76 @@ Doctor CreateDoctor__ChildrenDepartment()
     string fullName = Console.ReadLine();
 
     return new Doctor(login, password, department, specialization, fullName);
+}
+
+bool AdminAccount(Admin admin, List <Doctor> doctorsList, bool trigger2)
+{
+    while (trigger2 != false)
+    {
+        Console.Clear();
+
+        Console.WriteLine(admin.FullName.ToUpper() + "\n");
+        Console.WriteLine("Что бы вы хотели сделать?\n" + "1. Посмотреть список врачей.\n" + "2. Добавить врача.\n" + "3. Удалить врача.\n" + "4. Поменять пароль.\n" + "5. Выйти из аккаунта");
+        int actionAdmin = Convert.ToInt32(Console.ReadLine());
+
+        switch (actionAdmin)
+        {
+            case 1:
+                Console.Clear();
+
+                if (doctorsList.Count > 0)
+                {
+                    foreach (Doctor doctor in doctorsList)
+                    {
+                        Console.WriteLine($"{doctor.FullName}  |  {doctor.Department}  |  {doctor.Specialization}\n");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Список врачей пуст. Пора принимать на работу!");
+                }
+
+                Console.WriteLine("Нажмите 1, чтобы выйти.");
+                string exit = Console.ReadLine();
+
+                break;
+
+
+            case 2:
+                Console.Clear();
+
+                if (admin.Department == "Взрослое отделение")
+                {
+                    Doctor doctor = CreateDoctor__AdultDepartment();
+                    doctorsList.Add(doctor);
+                }
+                else if (admin.Department == "Детское отделение")
+                {
+                    Doctor doctor = CreateDoctor__ChildrenDepartment();
+                    doctorsList.Add(doctor);
+                }
+
+
+                break;
+
+            case 3:
+                Console.Clear();
+
+                break;
+
+            case 4:
+                Console.Clear();
+
+                break;
+
+            case 5:
+                trigger2 = false;
+                Console.Clear();
+                break;
+
+        }
+    }
+
+    return false;
 }
